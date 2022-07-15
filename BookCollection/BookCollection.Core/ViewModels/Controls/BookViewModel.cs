30d6be874b6f601f2c079343fd5832f0390db91b
nejski <wojtalokt@gmail.com>
2022-07-15 15:39:59 +0200

    Created default layout

---
+++ BookCollection/BookCollection.Core/ViewModels/Controls/BookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCollection.Core.ViewModels.Controls
+{
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ BookCollection/BookCollection.Core/ViewModels/Pages/BooksPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BookCollection.Core.ViewModels.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCollection.Core.ViewModels.Pages
+{
+    public class BooksPageViewModel
+    {
+        public List<BookViewModel> Books { get; set; } = new List<BookViewModel>();
+
+        public string NewBookTitle { get; set; }
+        public string NewBookAuthor { get; set; }
+
+        private void AddNewBook()
+        {
+            var newBook = new BookViewModel
+            {
+                Title = NewBookTitle,
+                Author = NewBookAuthor,
+            };
+
+            Books.Add(newBook);
+        }
+    }
+}
